e940c345a0415b134751866ae7f059caa5f215e6
Chigo55 <jungih456@gmail.com>
2025-08-08 10:54:58 +0900

    Feat: Add hyperparameters in config file

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import random
-
+import torch
 from engine import LightningEngine
 from model.model import *
 
-import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,0"
-
+torch.set_float32_matmul_precision(precision='high')
 
 def get_hparams():
     hparams = {
         # 모델 구조
-        "image_size": 512,
-        "offset": 0.5,
-        "cutoff": 0.1,
+        "in_channels": 3,
+        "out_channels": 3,
+        "hidden_channels": 64,
+        "num_levels": 4,
+        "temb_dim": 64,
+        "dropout_ratio": 0.1,
+        "filter_size": 5,
+        "sigma": 1.0,
+        "omega_x": 0.25,
+        "omega_y": 0.25,
+        "squeeze_ratio": 0.3,
+        "shortcut": True,
+        "trainable": False,
 
-        # 손실 함수 가중치 (losses.py 기준)
-        "lambda_col": 100.0,
-        "lambda_exp": 1.0,
-        "lambda_spa": 10.0,
-        "lambda_tva": 1.0,
+        # 데이터 모듈
+        "train_data_path": "data/1_train",
+        "valid_data_path": "data/2_valid",
+        "bench_data_path": "data/3_bench",
+        "infer_data_path": "data/4_infer",
+        "image_size":256,
+        "batch_size": 1,
+        "num_workers": 10,
+
+        # 엔진
+        "max_epochs": 100,
+        "accelerator": "gpu",
+        "devices": 1,
+        "precision": 32,
+        "log_every_n_steps": 5,
+        "gradient_clip_val": 0.1,
+        "save_dir": "./runs/HomomorphicUnet/optims/st",
+        "experiment_name": "test",
+
+        # 모델 모듈
+        "device": "cuda",
+        "timesteps": 1000,
 
         # 최적화 및 학습 설정
         "optim": "sgd",
@@ -28,7 +52,7 @@ def get_hparams():
         "epochs": 100,
         "patience": 30,
         "batch_size": 16,
-        "seed": 7077,
+        "seed": 42,
 
         # 데이터 경로
         "train_data_path": "data/1_train",
@@ -37,7 +61,7 @@ def get_hparams():
         "infer_data_path": "data/4_infer",
 
         # 로깅 설정
-        "log_dir": "./runs/HomomorphicUnet/optims/st",
+        "log_dir": "./runs/HomomorphicUnet/optims",
         "experiment_name": "test",
         "inference": "inference",
     }
@@ -54,20 +78,14 @@ def main():
         hparams["optim"] = opt
         hparams["experiment_name"] = opt
 
-        try:
-            engin = LightningEngine(
-                model=HomomorphicUnetLightning,
-                hparams=hparams
-            )
-            engin.train()
+        engin = LightningEngine(
+            model=ContourletDiffusionLightning,
+            hparams=hparams
+        )
 
-        except RuntimeError as e:
-            print(f"[NaN DETECTED] Optimizer '{opt}' 에서 학습 중단됨: {e}")
-            engin.update_ckpt_from_nan()
-
-        # 학습 성공 또는 NaN 발생 후 복구된 상태로 평가
-        engin.valid()
-        engin.bench()
+        engin.train()
+        # engin.valid()
+        # engin.bench()
         # engin.infer()
 
 
